e3c5b7010a677fa1e4224320560ded0c4152852a
harshitha <2500031041@kluniversity.in>
2026-03-03 11:15:40 +0530

    added demo.js by main

--- src/customer/BookEvent.jsx
+++ src/customer/BookEvent.jsx
@@ -1,4 +1,4 @@
- React, { useState, useEffect } from 'react';
+ import React, { useState, useEffect } from 'react';
 
 export default function BookEvent() {
   const [events, setEvents] = useState([]);
